c2308e18f1edcf3877edc34fce48dee07a2e23b7
sumit <skkushwaha@netlink.com>
2022-10-15 14:12:59 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import AnanlogClock from '../src/Component/AnanlogClock'
+import DigitalClock from '../src/Component/DigitalClock'
+import { useState } from 'react';
+import Clock from './Component/Clock';
 
 function App() {
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <AnanlogClock />
+      {/* <Clock /> */}
+      </div>
+
+      <div className='digitalclock' style={{background:'grey',borderRadius:'50%',
+      padding:'40px',marginLeft:'70vh',marginRight:'70vh',
+      textAlign:'left',marginTop:'-20%'}}>
+      <DigitalClock />
+      </div>
+      
     </div>
   );
 }
